48db8372edefef2376325d4e082f741523233f00
Fasilfaz <fazsby2001@gmail.com>
2025-02-11 10:54:25 +0530

    changed

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const app = express();
 
 //middlewares
 app.use(cors({
-    origin: "https://medico-eight.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
