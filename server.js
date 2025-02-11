bf19a2fdf3d375dad2ca8d462573f9d92a17e362
Fasilfaz <fazsby2001@gmail.com>
2025-02-11 11:03:53 +0530

    server updated

--- server.js
+++ server.js
@@ -2,65 +2,47 @@ import express from 'express';
 import dotenv from "dotenv";
 import {connectDb} from "./config/db.js";
 import userRouter from './routes/userRouter.js';
-// import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import doctorRouter from './routes/doctorRouter.js';
 import adminRouter from './routes/adminRouter.js';
 dotenv.config();
 
-//rest object
 const app = express();
 
-// var whitelist = ['http://localhost:5173', 'http://localhost:5000']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   },
-//   Credentials: true, 
-// };
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-//   });
-
-//middlewares
+// CORS configuration
 app.use(cors({
-    origin: "https://medico-eight.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: ['https://medico-eight.vercel.app', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
     credentials: true,
+    preflightContinue: false,
     optionsSuccessStatus: 204
 }));
-app.use(express.json());
-// app.use(cookieParser());
 
-
-
-
-
-//routes
-app.use("/api/v1/user",userRouter );
-app.use("/api/v1/doctor",doctorRouter);
-app.use("/api/v1/admin",adminRouter);
-
-//port
-const port = 3000;
-
-//db connection
-connectDb();
-//routes
-app.get('/', (req, res) => {
-    res.status(200).send({
-         message: "server running"
-    });
+// Add headers for additional CORS support
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Expose-Headers', 'Date, Etag');
+    next();
 });
 
-//listen port
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/doctor", doctorRouter);
+app.use("/api/v1/admin", adminRouter);
+
+const port = process.env.PORT || 3000;
+
+// DB connection
+connectDb();
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: "server running"
+    });
+});
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
